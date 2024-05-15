4d461d1c45d2aca151a0d35de05b4d1ef17d8cc1
laurenbgod <lauren.godwin@outlook.com>
2024-05-14 20:53:46 -0400

    Debugged

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const onlySection = document.querySelector("#section");
 const myList = onlySection.querySelector("ul");
-const repositories = json.parse;
+console.log(onlySection);
+console.log(myList);
 
-fetch(api_key=live_59tILUwO8GUppmwJdzaXpbh457s0r1o79I4ngwX8BU4T8vzHtQ1CgKytNapcRhCN)
-    .then((res) => {
-        if (!res.ok) {
-        throw new Error("Something went wrong 😢");
-        }
-        return res.json();
-    })
- 
-    .catch((error) => {
-        const errorElement = document.createElement("p");
-        errorElement.innerText = error.message;
-        onlySection.appendChild(errorElement);
+fetch(
+  "https://api.thedogapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_59tILUwO8GUppmwJdzaXpbh457s0r1o79I4ngwX8BU4T8vzHtQ1CgKytNapcRhCN"
+)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Something went wrong 😢");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    const dogUrl = data[0].url;
+    const dogImage = document.createElement("img");
+    dogImage.src = dogUrl;
+    myList.appendChild(dogImage);
+    console.log(dogUrl);
+    console.log(data);
+  })
+
+  .catch((error) => {
+    const errorElement = document.createElement("p");
+    errorElement.innerText = error.message;
+    onlySection.appendChild(errorElement);
   });
-
-    
